54e38c6b5c53686dca12621b8a4ecd5619ebd795
mhh <mike.hukiewitz@robotter.ai>
2023-11-21 13:56:11 +0100

    Feature: Summon Hugo's Ghost from the Nether to haunt developer's commit messages
    By analyzing a PRs commit messages with CodeLlama, the ghost will comment on the quality of the commit messages and urge the developer to improve them upon every push.

--- main.py
+++ main.py
@@ -42,6 +42,25 @@ def get_diff(repo_name, pr, access_token):
         print('Failed to retrieve diff:', response.status_code)
 
 
+def get_commit_messages(repo_name, pr, access_token) -> list:
+    # The GitHub API URL for the pull request commits
+    url = f'https://api.github.com/repos/{repo_name}/pulls/{pr}/commits'
+
+    # Set up the headers to authenticate with your token
+    headers = get_auth_header(access_token)
+
+    # Make the GET request to the GitHub API
+    response = requests.get(url, headers=headers)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        commits = response.json()
+        commit_messages = [commit['commit']['message'] for commit in commits]
+        return commit_messages
+    else:
+        print('Failed to retrieve commit messages:', response.status_code)
+
+
 def get_repo_desc(repo_name, access_token):
     # The GitHub API URL for the repository details
     url = f'https://api.github.com/repos/{repo_name}'
@@ -115,8 +134,7 @@ def query_llama(prompt: str, max_retries=3, delay=2) -> str:
     data = {
         'prompt': prompt,
         'stream': False,
-        'temperature': 0.0,
-        'min_p': 0.1,
+        'temperature': 0.3,
     }
 
     for attempt in range(max_retries):
@@ -140,85 +158,6 @@ def query_llama(prompt: str, max_retries=3, delay=2) -> str:
                 time.sleep(delay)  # Wait for some time before retrying
             else:
                 return f'Failed to retrieve llama text: {e}'
-
-
-def query_and_parse_llama(prompt) -> (Optional[Category], str):
-    llama_text = query_llama(prompt)
-    # Find the first line with either 'blue', 'red' or 'black' in it
-    category = None
-    comment = None
-    for i, line in enumerate(llama_text.split('\n')):
-        if Category.BLUE.value in line or Category.RED.value in line or Category.BLACK.value in line.upper():
-            if Category.BLUE.value in line:
-                category = Category.BLUE
-            elif Category.RED.value in line:
-                category = Category.RED
-            elif Category.BLACK.value in line.upper():
-                category = Category.BLACK
-            comment = '\n'.join(llama_text.split('\n')[i+1:])
-            break
-    if not category:
-        return None, llama_text
-    return category, comment
-
-
-def create_label_if_not_exists(repo_name, label_name, label_color, access_token):
-    headers = get_auth_header(access_token)
-
-    # Check if the label already exists
-    labels_url = f"https://api.github.com/repos/{repo_name}/labels"
-    response = requests.get(labels_url, headers=headers)
-
-    if response.status_code == 200:
-        labels = response.json()
-        label_names = [label["name"] for label in labels]
-
-        if label_name not in label_names:
-            # Create the label
-            if label_name == "BLUE":
-                description = "This PR is simple and straightforward."
-            elif label_name == "RED":
-                description = "This PR is complex and may require more time to review."
-            elif label_name == "BLACK":
-                description = "This PR has critical implications and must be reviewed by a senior engineer."
-            else:
-                description = None
-
-            data = {
-                "name": label_name,
-                "color": label_color,
-                "description": description
-            }
-            create_response = requests.post(labels_url, json=data, headers=headers)
-
-            if create_response.status_code == 201:
-                print(f"Label '{label_name}' created successfully.")
-            else:
-                print(f"Failed to create label '{label_name}': {create_response.content}")
-        else:
-            pass
-    else:
-        print(f"Failed to retrieve labels: {response.content}", labels_url)
-
-
-def add_label_to_pr(repo, pr, label, access_token):
-    headers = get_auth_header(access_token)
-
-    # GitHub API URL for issue labels (PRs are considered issues in the API)
-    labels_url = f"https://api.github.com/repos/{repo}/issues/{pr}/labels"
-
-    # Data to add the label
-    data = {
-        "labels": [label]
-    }
-
-    # Make the POST request to add the label to the PR
-    response = requests.post(labels_url, json=data, headers=headers)
-
-    if response.status_code == 200:
-        print(f"Label '{label}' added to PR #{pr_number} successfully.")
-    else:
-        print(f"Failed to add label '{label}' to PR #{pr_number}: {response.content}", labels_url)
 
 
 def get_auth_header(access_token, accept_header="application/vnd.github.v3+json"):
@@ -259,17 +198,19 @@ def add_comment_to_pr(repo_name, pr, comment_body, access_token):
 
 
 repo_desc = get_repo_desc(repo_name, github_token)
-diff = get_diff(repo_name, pr_number, github_token)
-rules = get_rules(repo_name, github_token)
+pr_title, pr_desc = get_pr_desc(repo_name, pr_number, github_token)
+commit_messages = get_commit_messages(repo_name, pr_number, github_token)
+message_list = "\n".join(commit_messages)
 
 user_prompt = f"""
 --- {repo} ---
 {repo_desc}
 
-Rules: {rules}
+--- PR #{pr_number}: {pr_title} ---
+{pr_desc}
 
-Diff:
-{diff}
+--- Commit Messages ---
+{message_list}
 """
 
 with open("system_prompt.txt", "r") as f:
@@ -286,24 +227,9 @@ full_prompt = f"""### System Prompt
 
 print(f"Prompt: {full_prompt}")
 
-category, comment = query_and_parse_llama(full_prompt)
+comment = query_llama(full_prompt)
 
-print(f"Category: {category}")
 print(f"Comment: {comment}")
 
 if not dry_run:
-    # Define labels and their respective colors
-    labels = {
-        "BLUE": "2A3EDD",
-        "RED": "DD2A2A",
-        "BLACK": "000000"
-    }
-    # Create each label if it does not exist
-    for label, color in labels.items():
-        create_label_if_not_exists(repo_name, label, color, github_token)
-    if category:
-        add_label_to_pr(repo_name, pr_number, category.value, github_token)
-        add_comment_to_pr(repo_name, pr_number, comment, github_token)
-    else:
-        print(f"Could not find category in response: {comment}")
-        add_comment_to_pr(repo_name, pr_number, comment, github_token)
+    add_comment_to_pr(repo_name, pr_number, comment, github_token)
